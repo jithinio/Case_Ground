338f36783d0f47e94cf9f3693bbecf39956a7da2
jithinio <hello@jithin.io>
2023-06-01 18:35:23 +0530

    Adding Text Caps Converter & Clear Fields

--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
+// Global Variable
+let mainInputBox = document.getElementById("inputbox");
+
 // Char Counter
-let charCounter = document.getElementById("inputbox");
 let characterCounter = document.getElementById("charCounterText");
 let countCharacters = () => {
-  let charCount = charCounter.value.length;
+  let charCount = mainInputBox.value.length;
   characterCounter.textContent = charCount;
 };
+mainInputBox.addEventListener("input", countCharacters);
 
 // Word Counter
-let wordCount = document.getElementById("inputbox");
 let wordCounter = document.getElementById("wordCounterText");
 let totalWords = () => {
-  let wordLength = wordCount.value.split(/[^\s]+/).length - 1;
+  let wordLength = mainInputBox.value.split(/[^\s]+/).length - 1;
   wordCounter.textContent = wordLength;
 };
+mainInputBox.addEventListener("input", totalWords);
 
-// Counters Live Event Handlers
-wordCount.addEventListener("input", totalWords);
-charCounter.addEventListener("input", countCharacters);
+// Clipboard Functions
+let clipBoardButton = document.getElementById("clipboardbutton");
+// Initiating Clipboardjs
+let clipboard = new ClipboardJS(clipBoardButton);
+
+clipBoardButton.addEventListener("click", (clipboard) => {
+  let alertBadge = document.getElementById("copiedtextbadge");
+  alertBadge.style.display = "block";
+  setTimeout(() => {
+    alertBadge.style.display = "none";
+  }, 2000);
+});
+
+// Clear text
+let clearText = document.getElementById("cleartext");
+clearText.addEventListener("click", () => {
+  mainInputBox.value = "";
+  let clearWord = document.getElementById("wordCounterText");
+  clearWord.value = "";
+  wordCounter.innerText = "0";
+  characterCounter.innerText = "0";
+});
+
+// Sentence convert.
+let convertButton = document.getElementById("convertbutton");
+convertButton.addEventListener("click", () => {
+  let toConverter = mainInputBox.value;
+  let sentences = String(toConverter).split(/(?<=[.!?])\s+/);
+  let convertedSentense = sentences
+    .map((item) => item.charAt(0).toUpperCase() + item.slice(1).toLowerCase())
+    .join(" ");
+  mainInputBox.value = convertedSentense;
+});
